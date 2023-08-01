5a94c67f14a4fdf7b1e74477a6f3b81db3862d2d
mixtuti桜餅 <88471532+mixtuti@users.noreply.github.com>
2023-08-01 17:02:31 +0900

    Add files via upload

--- js/pw.js
+++ js/pw.js
@@ -1,11 +1,26 @@
-// JavaScript Document
+// // JavaScript Document
+// function gate(){
+//    // ▼ユーザの入力を求める
+//    var UserInput = prompt("パスワードを入力して下さい:","");
+//    // ▼入力内容をチェック
+//    if( /\W+/g.test(UserInput) ) {
+//       // ▼半角英数字以外の文字が存在したらエラー
+//       alert("半角英数字のみを入力して下さい。");
+//    }
+//    // ▼キャンセルをチェック
+//    else if( UserInput != null ) {
+//       // ▼入力内容からファイル名を生成して移動
+//       location.href = UserInput + ".html";
+//    }
+// }
+
 function gate(){
    // ▼ユーザの入力を求める
    var UserInput = prompt("パスワードを入力して下さい:","");
    // ▼入力内容をチェック
-   if( /\W+/g.test(UserInput) ) {
-      // ▼半角英数字以外の文字が存在したらエラー
-      alert("半角英数字のみを入力して下さい。");
+   if( /[^a-zA-Z0-9\-]+/g.test(UserInput) ) {
+      // ▼半角英数字とハイフン以外の文字が存在したらエラー
+      alert("半角英数字とハイフンのみを入力して下さい。");
    }
    // ▼キャンセルをチェック
    else if( UserInput != null ) {
