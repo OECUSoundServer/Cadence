ed29579e9dba779b44b1997f2548f4011836a7cc
mixtuti桜餅 <88471532+mixtuti@users.noreply.github.com>
2023-08-28 11:15:07 +0900

    Add files via upload

--- js/serch.js
+++ js/serch.js
@@ -1,3 +1,5 @@
+// 没
+
 let currentSortColumn = null;
 let currentSortOrder = 1; // 1: 昇順, -1: 降順
 
@@ -130,3 +132,30 @@ function searchData() {
 
 // ページ読み込み時に外部データを読み込む
 loadExternalData();
+
+// リアルタイムでテーブルデータを検索して表示する関数
+// function searchData() {
+//     const searchInput = document.getElementById("searchInput").value.toLowerCase();
+//     const table = document.getElementById("albumTable");
+//     const rows = table.getElementsByTagName("tr");
+
+//     for (let i = 1; i < rows.length; i++) { // i = 1 から開始してヘッダー行をスキップ
+//         const cells = rows[i].getElementsByTagName("td");
+//         let found = false;
+
+//         for (let j = 1; j < cells.length -1; j++) { // アルバム名からアーティスト名まで検索
+//             const cellText = cells[j].textContent.toLowerCase();
+//             if (cellText.includes(searchInput)) {
+//                 found = true; // 一致するデータが見つかったらtrueを返す
+//                 break;
+//             }
+//         }
+
+//         if (found) {
+//             rows[i].style.display = "";
+//         }
+//         else {
+//             rows[i].style.display = "none";
+//         }
+//     }
+// }
