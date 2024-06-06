d831776778ef07da5e51037bfe5ed57394f7b063
mixtuti桜餅 <88471532+mixtuti@users.noreply.github.com>
2024-06-06 09:53:47 +0900

    Update pw.js

--- js/pw.js
+++ js/pw.js
@@ -14,17 +14,47 @@
 //    }
 // }
 
+// function gate(){
+//    // ▼ユーザの入力を求める
+//    var UserInput = prompt("パスワードを入力して下さい:","");
+//    // ▼入力内容をチェック
+//    if( /[^a-zA-Z0-9\-]+/g.test(UserInput) ) {
+//       // ▼半角英数字とハイフン以外の文字が存在したらエラー
+//       alert("半角英数字とハイフンのみを入力して下さい。");
+//    }
+//    // ▼キャンセルをチェック
+//    else if( UserInput != null ) {
+//       // ▼入力内容からファイル名を生成して移動
+//       location.href = UserInput + ".html";
+//    }
+// }
+
 function gate(){
    // ▼ユーザの入力を求める
-   var UserInput = prompt("パスワードを入力して下さい:","");
+   var userInput = prompt("パスワードを入力して下さい:","");
    // ▼入力内容をチェック
-   if( /[^a-zA-Z0-9\-]+/g.test(UserInput) ) {
+   if( /[^a-zA-Z0-9\-]+/g.test(userInput) ) {
       // ▼半角英数字とハイフン以外の文字が存在したらエラー
       alert("半角英数字とハイフンのみを入力して下さい。");
    }
    // ▼キャンセルをチェック
-   else if( UserInput != null ) {
-      // ▼入力内容からファイル名を生成して移動
-      location.href = UserInput + ".html";
+   else if( userInput !== null ) {
+      // ▼入力内容からファイル名を生成
+      var fileName = userInput + ".html";
+      // ▼ページが存在するかチェック
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+         if (this.readyState == 4) {
+            if (this.status == 200) {
+               // ▼ページが存在する場合は移動
+               location.href = fileName;
+            } else {
+               // ▼ページが存在しない場合は警告を表示
+               alert("ページが存在しません。");
+            }
+         }
+      };
+      xhttp.open("GET", fileName, true);
+      xhttp.send();
    }
 }
